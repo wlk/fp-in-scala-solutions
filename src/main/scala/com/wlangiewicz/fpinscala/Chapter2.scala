c953a197dcfa27af1b6e5390f51c1200f39abf7f
Wojciech Langiewicz <wlangiewicz@gmail.com>
2017-04-13 21:11:26 +0200

    more tests

--- src/main/scala/com/wlangiewicz/fpinscala/Chapter2.scala
+++ src/main/scala/com/wlangiewicz/fpinscala/Chapter2.scala
@@ -55,6 +55,8 @@ object exercise_2_2 extends App {
   }
 
   println(isSorted[Int](Array(1, 2, 3, 4, 5, 6), (a, b) => a >= b))
+  println(isSorted[Int](Array(1), (a, b) => a <= b))
+  println(isSorted[Int](Array(1,2), (a, b) => a <= b))
   println(isSorted[Int](Array(1, 5, 3, 7, 2, 4, 9, 2), (a, b) => a >= b))
   println(isSorted[Int](Array(1, 2, 3, 4, 5, 6), (a, b) => a < b))
   println(isSorted[Int](Array.empty, (a, b) => a < b))
